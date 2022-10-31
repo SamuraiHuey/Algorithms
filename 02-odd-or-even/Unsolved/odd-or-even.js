f4faa17132931348b643a5935af0221408ecc055
Will B <tryarc@comcast.net>
2022-10-31 13:48:00 -0400

    Halloween push

--- 02-odd-or-even/Unsolved/odd-or-even.js
+++ 02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,12 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
+var num = 3
 
-var oddOrEven = function(num) {};
+var oddOrEven = function(num) {
+    if (num % 2 == 0){
+        return 'even';
+    }
+    else {
+        return 'odd';
+    }
+};
--- 06-sum-array/Unsolved/sum-array.js
+++ 06-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,12 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    var sum = 0;
+  
+    for (var i = 0; i < arr.length; i++) {
+      var currentNumber = arr[i];
+      sum += currentNumber;
+    }
+  
+    return sum;
+  };
